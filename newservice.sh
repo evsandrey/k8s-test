4acf55611676ead99635147274dff0e57d873583
evsandrey <evs.andrey@gmail.com>
2019-03-16 11:48:40 +0300

    kuber configs added

--- newservice.sh
+++ newservice.sh
@@ -2,9 +2,9 @@ cp -r boilerplate/ $1
 sed -i "s/boilerplate/$1/g" $1/config.sh
 sed -i "s/boilerplate/$1/g" $1/app/package.json
 
-cp -r _k8s/boilerplate-deployment.yml _k8s/$1-deployment.yml
-sed -i "s/boilerplate/$1/g" _k8s/$1-deployment.yml
+cp -r $1/_k8s/boilerplate-deployment.yml $1/_k8s/$1-deployment.yml
+sed -i "s/boilerplate/$1/g" $1/_k8s/$1-deployment.yml
 
-cp -r _k8s/boilerplate-client-ip-service.yml _k8s/$1-client-ip-service.yml
-sed -i "s/boilerplate/$1/g" _k8s/$1-client-ip-service.yml
+cp -r $1/_k8s/boilerplate-client-ip-service.yml $1/_k8s/$1-client-ip-service.yml
+sed -i "s/boilerplate/$1/g" $1/_k8s/$1-client-ip-service.yml
 
